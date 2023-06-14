04a4daf4846f20f4975202702ae0bb1871711df0
d0hyun07 <128461034+d0hyun07@users.noreply.github.com>
2023-06-14 10:30:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from flask import *
-import websocket
-import time
+from flask import * # flask 프레임워크 import (웹서버)
+import websocket # websocket 모듈 import (아두이노 서버와 통신을 위해서..)
 
 app = Flask(__name__)
-onOff = "on"
-num_state = '1'
+onOff = "on" # 웹사이트에 보여지는 상태값 on off
+num_state = '1' # 실제 아두이노 서버에 보내지는 상태값 1 = on 0 = off
 
-@app.route("/",methods=["GET"])
+@app.route("/",methods=["GET"]) # root 경로 접속
 def index():
-  if request.method == "GET":
-    print(onOff)
-    return render_template("index.html",state = onOff)
+  if request.method == "GET": # get 메소드로 접속
+    return render_template("index.html",state = onOff) # index.html로 state값 출력
   
 @app.route("/state",methods=["POST"])
 def state():
-  if request.method == "POST":
+  if request.method == "POST": # post로 보냈을 때
     global num_state
-    ws = websocket.WebSocket()
-    ws.connect ("ws://10.150.151.142/")
+    ws = websocket.WebSocket() 
+    ws.connect ("ws://10.150.151.142/") # 아두이노 서버 연결
 
-    onOff = request.form["onOff"]
-    ws.send(num_state)
-    result = ws.recv()
+    onOff = request.form["onOff"] # 사용자가 입력한 버튼 값 (state)
+    ws.send(num_state) state 값 전송
+    result = ws.recv() 
     print (result)
 
     ws.close()
@@ -34,4 +32,4 @@ def state():
 
   return render_template("index.html",state = onOff)
 if __name__ == '__main__':
-  app.run(port=3000,debug=True)
+  app.run(port=3000,debug=True) # port 번호 3000
